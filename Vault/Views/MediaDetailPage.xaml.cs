a5fbbc3ad359191d1105e9a6ba584a10f088fde6
Lunarius69 <aleksasd999@gmail.com>
2026-03-16 14:14:31 +0100

    fixed episode click bug

--- Vault/Views/MediaDetailPage.xaml.cs
+++ Vault/Views/MediaDetailPage.xaml.cs
@@ -326,10 +326,10 @@ namespace Vault.Views
         }
 
         private async void EpisodeCard_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button btn && btn.Tag is EpisodeViewModel vm)
-                await PlayEpisodeAsync(vm.Episode);
-        }
+{
+    if (sender is Button btn && btn.DataContext is EpisodeViewModel vm)
+        await PlayEpisodeAsync(vm.Episode);
+}
 
         private async Task PlayEpisodeAsync(Episode episode)
         {
